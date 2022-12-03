7c0e5066de95b55df09889fc8f30ef8e7e3721e6
damir <dimayu82@gmail.com>
2022-12-03 14:25:10 +0300

    Start project and creating a structure

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import AppInfo from "../AppInfo/AppInfo";
+
+function App() {
+    return (
+        <div className="app">
+            <AppInfo />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/AppInfo/AppInfo.js
@@ -0,0 +1,13 @@
+import './AppInfo.css';
+
+const AppInfo = () => {
+    return (
+        <header>
+            <h1>Учет сотрудников в компании N</h1>
+            <h2>Общее число сотрудников: </h2>
+            <h3>Премию получат: </h3>
+        </header>
+    );
+}
+
+export default AppInfo;
